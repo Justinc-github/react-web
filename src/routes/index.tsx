d44ae17de339d19104261ca8d4c240d2eda07a05
justinc <1927705375@qq.com>
2025-05-30 14:03:23 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ import Page2025_05_03 from "../pages/Log/pages/Page2025_05_03";
 import Music from "../pages/Music";
 import RequireAuth from "../pages/Auth/components/requireAuth";
 import Login from "../pages/Auth";
-import Register from "../static/pages/register";
+import Register from "../static/pages/register.tsx";
 export default [
   {
     path: "/",
